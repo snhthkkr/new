aec744fd575368379e02bed4aea45a3e73408cac
snhthkkr <99010309+snhthkkr@users.noreply.github.com>
2026-01-08 21:36:01 +0530

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,18 @@
+const CACHE = 'atem-v1';
+const ASSETS = ['/', '/index.html', '/manifest.json', '/icon-192.png', '/icon-512.png'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(ks => Promise.all(ks.filter(k => k !== CACHE).map(k => caches.delete(k)))));
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request).then(res => {
+    return caches.open(CACHE).then(c => { c.put(e.request, res.clone()); return res; });
+  })).catch(() => caches.match('/index.html')));
+});
