f3d629cdf6d854502387d9681ecd3b67c3d1cf1a
snhthkkr <99010309+snhthkkr@users.noreply.github.com>
2026-02-11 01:22:10 +0530

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,21 +1,8 @@
-const CACHE = 'atem-v5';
+const CACHE = 'atem-v6';
 const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
-
-self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
-  self.skipWaiting();
-});
-
-self.addEventListener('activate', e => {
-  e.waitUntil(caches.keys().then(ks => Promise.all(ks.filter(k => k !== CACHE).map(k => caches.delete(k)))));
-  self.clients.claim();
-});
-
+self.addEventListener('install', e => { e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS))); self.skipWaiting(); });
+self.addEventListener('activate', e => { e.waitUntil(caches.keys().then(ks => Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k))))); self.clients.claim(); });
 self.addEventListener('fetch', e => {
   if (!e.request.url.startsWith(self.location.origin)) return;
-  e.respondWith(
-    caches.match(e.request).then(r => r || fetch(e.request).then(res => {
-      return caches.open(CACHE).then(c => { c.put(e.request, res.clone()); return res; });
-    })).catch(() => caches.match('/index.html'))
-  );
+  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request).then(res => caches.open(CACHE).then(c => { c.put(e.request, res.clone()); return res; }))).catch(() => caches.match('/index.html')));
 });
